76aa30a0dde1150297a059a436129a7848f796bd
Sachinn-Sabu <sachinjake18@gmail.com>
2022-03-06 11:48:55 +0530

    commit

---
+++ PHP/replace.php
@@ -0,0 +1,6 @@
+<?php
+//input string
+$string="Hardwork is Perfection";
+//replace geeks in the string with computer
+echo str_replace("Perfection","Success",$string);
+?>
---
+++ PHP/subtractform.php
@@ -0,0 +1,19 @@
+<body>  
+<form>       
+Enter First Number:  
+<input type="number" name="number1" /><br><br>  
+Enter Second Number:  
+<input type="number" name="number2" /><br><br>  
+<input  type="submit" name="submit" value="Subtract">  
+</form>  
+ </body>  
+<?php  
+      
+@$number1=$_GET['number1'];   
+@$number2=$_GET['number2'];  
+for ($i=1; $i<=$number2; $i++)     
+{      
+ $number1--;      
+}      
+echo "Difference=".$number1;  
+?>  
